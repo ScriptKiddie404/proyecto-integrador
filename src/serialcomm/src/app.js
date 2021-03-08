8b20d5200c0eae818284e687abd7d133e536557b
ScriptKiddie404 <fernando.puentes.developer@gmail.com>
2021-03-07 19:21:57 -0700

    agregando contenido de SerialComm

--- src/serialcomm/src/app.js
+++ src/serialcomm/src/app.js
@@ -1,1 +1,20 @@
-const SerialCommunication = require('./serialcomm');
+//Chalk es usada sólo para brindar estética a la salida de consola
+const chalk = require('chalk');
+const okColor = chalk.bgGreenBright.black;
+
+
+// Serial Port communication
+const SerialPort = require('serialport');
+const Readline = require('@serialport/parser-readline');
+const port = new SerialPort('COM3', { baudRate: 9600 });
+const parser = port.pipe(new Readline({ delimiter: '\n' }));
+
+port.on("open", () => {
+    console.clear();
+    console.log(okColor("Comunicación serializada iniciada..."));
+});
+
+parser.on('data', data => {
+    console.log(data);
+});
+
