a9f9f6694f8da849dc1e6b7a0bdda453f7bf374f
ScriptKiddie404 <fernando.puentes.developer@gmail.com>
2021-03-11 15:47:19 -0700

    creating api

--- src/server/app.js
+++ src/server/app.js
@@ -1,17 +1,20 @@
+console.log(__dirname + '/src/public');
+
 // Se importa la base de datos para la conexión:
 require('./src/db/mongoose');
+const { response } = require('express');
 // Imports para el servidor:
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const PORT = process.env.PORT || 3000;
-
 // Import Routers:
 const sesorRecordsRouter = require('./src/routes/sensorRecordRouter');
 
 // =================================================== //
 // =================== MIDDLEWARE ==================== //
 // =================================================== //
+app.use(express.static(__dirname + '/src/public'))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('tiny'));
@@ -19,6 +22,10 @@ app.use(morgan('tiny'));
 // =================================================== //
 // =================== ROUTERS ======================= //
 // =================================================== //
+app.get('/', async (req, res) => {
+    response.sendFile(__dirname + '/src/public/index.html');
+});
+
 app.use(sesorRecordsRouter);
 
 
--- src/server/src/routes/sensorRecordRouter.js
+++ src/server/src/routes/sensorRecordRouter.js
@@ -2,10 +2,6 @@ const express = require('express');
 const SensorRecord = require('../models/SensorRecord');
 const router = new express.Router();
 
-router.get('/', (req, res) => {
-    res.send('hola putito');
-});
-
 // All records
 router.get('/records', async (req, res) => {
     try {
