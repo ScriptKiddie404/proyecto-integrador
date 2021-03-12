ebfe684ecd117f6fe99a017d9f2ab81e6ff34dfe
ScriptKiddie404 <fernando.puentes.developer@gmail.com>
2021-03-11 18:44:24 -0700

    creating api

--- src/server/src/db/mongoose.js
+++ src/server/src/db/mongoose.js
@@ -1,8 +1,6 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-// mongodb+srv://fernando:nC.c-ZL3Qhc.rqY@cluster0.wgodv.mongodb.net/sensor-data?retryWrites=true&w=majority
-
 mongoose.connect(process.env.MONGODB_URI || `mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.wgodv.mongodb.net/sensor-data?retryWrites=true&w=majority`, {
     useNewUrlParser: true,
     useCreateIndex: true,
